a8c16d7ee9bfa8cadd050d7b7b6e7e900ac5eb32
yisui <yisui@math.ubc.ca>
2014-11-20 13:12:01 -0800

    test about vignettes

---
+++ inst/doc/overview.R
@@ -0,0 +1,48 @@
+## ------------------------------------------------------------------------
+library(gameday)
+
+## ------------------------------------------------------------------------
+# check whether "Canucks" has a game today
+gday()
+
+## ------------------------------------------------------------------------
+# check whether "Bruins" has a game on "2014-11-02"
+gday("Bruins", "2014-11-02")
+
+## ------------------------------------------------------------------------
+# apply gday() to "Canucks"
+gday("Canucks")
+
+# apply gday() to "canucks"
+gday("canucks")
+
+# confirm gday() is not case sensitive
+identical(gday("Canucks"), gday("canucks"))
+
+## ------------------------------------------------------------------------
+# apply gday() to "canucks"
+gday("canucks")
+
+# apply gday() to "Vancouver"
+gday("Vancouver")
+
+# confirm asking by the city name also works
+identical(gday("canucks"), gday("Vancouver"))
+
+## ------------------------------------------------------------------------
+# look up the final scores for the games on "2014-11-18"
+scores("2014-11-18")
+
+## ------------------------------------------------------------------------
+# load package "dplyr"
+suppressPackageStartupMessages(library(dplyr))
+
+## ------------------------------------------------------------------------
+# look for the winning teams  on "2014-11-18"
+scores_data <- scores("2014-11-18")
+
+scores_data %>%
+	mutate(winning_team = ifelse(home_score > away_score, as.character(home), 
+															as.character(away))) %>%
+	select(winning_team)
+
